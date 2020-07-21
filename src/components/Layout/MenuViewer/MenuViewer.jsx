f877eb9875fedd4af25f4c3b4b5f077565572642
amish1188 <martin.joodd@gmail.com>
2020-07-21 23:23:00 +0200

    split array

--- src/components/Layout/MenuViewer/MenuViewer.jsx
+++ src/components/Layout/MenuViewer/MenuViewer.jsx
@@ -3,7 +3,22 @@ import React from 'react';
 import Row from '../Row/Row';
 import classes from './MenuViewer.module.css';
 
-const MenuViewer = () => {
+const MenuViewer = (props) => {
+
+    const items = props.cookies
+    let firstRowItems = [];
+    let secondRowItems = [];
+
+
+
+    for(let i = 0; i< items.length; i++){
+        if(i % 2 === 0){firstRowItems.push(items[i])}
+        else{secondRowItems.push(items[i])}
+    }
+    console.log(firstRowItems);
+    
+    
+
     return(
         
             <div className={classes.MenuViewer}>
--- src/containers/CookiesMenu/CookiesMenu.jsx
+++ src/containers/CookiesMenu/CookiesMenu.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import MenuViewer from '../../components/Layout/MenuViewer/MenuViewer';
 class CookiesMenu extends Component {
+    state = {
+        cookies: []
+    };
+
+    componentDidMount() {
+        axios.get('https://cookie-app-80b62.firebaseio.com/cookies.json')
+        .then(response => {
+                this.setState({cookies: response.data})
+                console.log(this.state.cookies);
+            });
+    };
+
     render() {
+        let content = this.state.cookies ? <MenuViewer cookies={this.state.cookies}/> : null
         return(
-            
-                <MenuViewer />
-            
+            <React.Fragment>
+                {content}
+            </React.Fragment>
         )
     }
-}
+};
  
 export default CookiesMenu;
