fee54434887a7480ddc04095cec9a9ba8edfc016
amish1188 <martin.joodd@gmail.com>
2020-07-31 17:24:28 +0200

    remove repetitions from state

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -5,17 +5,24 @@ const initialState = {
     sum: 0
 };
 
+//function that checks if there are repetiotions in objects, and sum the quantity
+const addProduct = (state,action) => {
+    const index = state.products.findIndex(p => p.name === action.product.name);
+    if(index < 0){
+        return{ ...state, products: state.products.concat(action.product)}
+    } else{
+        const product = {...state.products[index]};
+        product.quantity += action.product.quantity;
+        const products = [...state.products];
+        products[index] = product;
+        return {...state, products: products}
+    }
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'ADD_TO_CART':
-            return{
-                ...state,
-                products: state.products.concat(action.product),
-                sum: state.sum + (action.product.price * action.product.quantity)
-            };
-           
-        }
-        return state;
-};
+        case 'ADD_TO_CART': return addProduct(state,action);
+        default: return state;
+}};
 
 export default reducer;
